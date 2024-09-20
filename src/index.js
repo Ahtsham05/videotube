834b35ba90d01aa062fa34245f231cd446368657
Ahtsham05 <ahtshamyounas0321@gmail.com>
2024-09-20 07:18:34 -0700

    Api Error Handling

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import dotenv from 'dotenv'
 import dbConnection from './db/index.js'
+import {app} from './app.js'
 dotenv.config({
     path:'./.env'
 })
-
+const port =process.env.PORT || 8000
 dbConnection()
 .then(()=>{
-    console.log('Database Connected Successfully !')
+    app.on("Error",(error)=>{
+        console.log("App Running Error !",error)
+    })
+    app.listen(port,()=>{
+        console.log("App listening on port ",port)
+    })
 })
 .catch((error)=>{
     console.log('Database Connect Failed', error)
